86948cdb2907d4ababd11a009411070cc002cbd0
Achlesy <achlesyman103@gmail.com>
2016-12-07 20:39:43 +0700

    Update Script js pemanggil.js

--- Script js pemanggil.js
+++ Script js pemanggil.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=null;
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=;
 if(x=="1"){xxx="/2016/12/redirects.html"}
 if(x=="2"){xxx="/2016/12/glass-wall-construction-for-exclusive.html"}
 if(x=="3"){xxx="/2016/12/stress-eating-helps-when-they.html"}
